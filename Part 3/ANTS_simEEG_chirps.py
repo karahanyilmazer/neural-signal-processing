5e3881dc0c13f2815174397ec7ab801a79b45ad1
Karahan Yilmazer <karahan.yilmazer@tum.de>
2022-12-22 19:19:40 +0100

    feat: Create bipolar and multipolar chirp signal

---
+++ Part 3/ANTS_simEEG_chirps.py
@@ -0,0 +1,62 @@
+# %%
+#     COURSE: Solved challenges in neural time series analysis
+#    SECTION: Simulating EEG data
+#      VIDEO: Generating "chirps" (frequency-modulated signals)
+# Instructor: sincxpress.com
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# %% SIMULATION DETAILS
+
+pnts = 10000
+srate = 1024
+time = np.arange(pnts) / srate
+
+# %% CHIRPS
+# Bipolar chirp
+freq_bi = np.linspace(5, 15, pnts)
+
+# Multipolar chirp
+k = 10  # poles for frequencies
+xi = np.arange(k)
+yi = np.random.rand(k)
+x = np.linspace(0, k - 1, pnts)
+y = np.interp(x, xi, yi)
+freq_mult = 20 * y
+
+# Signal time series
+signal_bi = np.sin(2 * np.pi * ((time + np.cumsum(freq_bi)) / srate))
+signal_mult = np.sin(2 * np.pi * ((time + np.cumsum(freq_mult)) / srate))
+
+# %% PLOTTING
+
+mosaic = [['freq', 'freq'], ['chirp', 'chirp']]
+fig = plt.figure(constrained_layout=True)
+ax_dict = fig.subplot_mosaic(mosaic)
+
+ax_dict['freq'].plot(time, freq_bi)
+ax_dict['freq'].set_title('Instantaneous Frequency')
+ax_dict['freq'].set_xlabel('Time (s)')
+ax_dict['freq'].set_ylabel('Frequency (Hz)')
+
+ax_dict['chirp'].plot(time, signal_bi)
+ax_dict['chirp'].set_title('Bipolar Chirp')
+ax_dict['chirp'].set_xlabel('Time (s)')
+ax_dict['chirp'].set_ylabel('Amplitude (a.u.)')
+
+mosaic = [['freq', 'freq'], ['chirp', 'chirp']]
+fig = plt.figure(constrained_layout=True)
+ax_dict = fig.subplot_mosaic(mosaic)
+
+ax_dict['freq'].plot(time, freq_mult)
+ax_dict['freq'].set_title('Instantaneous Frequency')
+ax_dict['freq'].set_xlabel('Time (s)')
+ax_dict['freq'].set_ylabel('Frequency (Hz)')
+
+ax_dict['chirp'].plot(time, signal_mult)
+ax_dict['chirp'].set_title('Multipolar Chirp')
+ax_dict['chirp'].set_xlabel('Time (s)')
+ax_dict['chirp'].set_ylabel('Amplitude (a.u.)')
+
+plt.show()
